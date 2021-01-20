5c6b62859440bcce46c04a79c81d9d15c7cfdfe7
Jennifer Banks <59672512+JenBanks8585@users.noreply.github.com>
2021-01-19 19:30:31 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,41 @@
 
+from fastapi import FastAPI, Depends
+import requests
+import uvicorn
+import json
+import pandas as pd
+import os
+from pydantic import BaseModel, SecretStr
+from dotenv import load_dotenv
+import config
+
+load_dotenv()
+
+description = """
+To use these interactive docs:
+- Click on an endpoint below
+- Click the **Try it out** button
+- Edit the Request body or any parameters
+- Click the **Execute** button
+- Scroll down to see the Server response Code & Details
+"""
+
+app = FastAPI(debug = True, 
+              title = "Realty API",
+             description = description,
+             docs_url = '/')
+ 
+  
+app.include_router(realty.router, tags=['Realty'])
+app.include_router(pollution.router, tags=['Pollution'])
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+if __name__ == '__main__':
+    uvicorn.run(app)
